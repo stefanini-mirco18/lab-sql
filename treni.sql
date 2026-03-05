b09e3d3628837429310d706e7527fed254514d35
stefanini-mirco18 <249056654+stefanini-mirco18@users.noreply.github.com>
2026-03-05 08:08:47 +0100

    Update treni.sql

--- treni.sql
+++ treni.sql
@@ -1,5 +1,5 @@
 create table treno (
-    ID_treno VARCHAR(6) PRIMARY KEY,
+    id_treno VARCHAR(6) PRIMARY KEY,
     max_posti INT(55) not null,
     anno_costruzione DATE not null
    );
@@ -12,7 +12,7 @@ create table tratta(
     );
 
 create table corsa(
-    ID_corsa INT(11) AUTO_INCREMENT PRIMARY KEY,
+    id_corsa INT(11) AUTO_INCREMENT PRIMARY KEY,
     code_tratta INT(11),
     FOREIGN KEY (code_tratta) REFERENCES tratta(code_tratta),
     ID_treno VARCHAR(55) not null,
